b71016cb223fd97e56d92729875100064c375c3c
mimi5963 <mimi5963@naver.com>
2024-02-26 18:51:26 +0900

    Fix:쿠키 httpOnly뺌

--- src/main/java/com/metamon/horok/config/secs/oauth/CookieUtils.java
+++ src/main/java/com/metamon/horok/config/secs/oauth/CookieUtils.java
@@ -34,7 +34,9 @@ public class CookieUtils {
     public static void addCookie(HttpServletResponse response, String name, String value, int maxAge) {
         Cookie cookie = new Cookie(name, value);
         cookie.setPath("/");
-        cookie.setHttpOnly(true);
+       
+//        cookie.setSecure(true);
+//        cookie.setDomain();
         cookie.setMaxAge(maxAge);
         response.addCookie(cookie);
     }
