fd99f57a5576163d693f33160dee40d06590a96a
taeyeon357 <teayun357@gmail.com>
2024-02-27 18:27:47 +0900

    Fix: 오류 수정

--- src/main/java/com/metamon/horok/controller/FoldersController.java
+++ src/main/java/com/metamon/horok/controller/FoldersController.java
@@ -72,10 +72,10 @@ public class FoldersController {
     }
 
     @GetMapping("/api/folders/notshared/{reviewId}")
-    public List<FolderDTO> getNotSharedFolderList(@UserIdFromJwt Integer userId, @PathVariable("reviewId") Integer reviewId){
+    public List<FolderDTO> getNotSharedFolderList(@UserIdFromJwt Integer userId, @PathVariable("reviewId") Integer reviewId) {
         System.out.println(userId);
         return folderService.findFolderIdsByUserIdAndReviewIdNotShared(userId, reviewId);
-
+    }
     @DeleteMapping("/api/folders/{folderId}")
     public String folderDelete(@UserIdFromJwt Integer userId, @PathVariable("folderId") Integer folderId){
         return folderService.deleteFolder(userId, folderId);
